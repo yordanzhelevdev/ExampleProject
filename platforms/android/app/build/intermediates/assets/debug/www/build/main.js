83544849d6607e4e5329716ab1c34b5c8cbd4f24
yordanzhelevdev <yordanzhelevdev@gmail.com>
2018-09-12 10:52:50 +0300

    rebuild for android 4

--- platforms/android/app/build/intermediates/assets/debug/www/build/main.js
+++ platforms/android/app/build/intermediates/assets/debug/www/build/main.js
@@ -50,31 +50,31 @@ var map = {
 		11
 	],
 	"../pages/item-create/item-create.module": [
-		333,
+		332,
 		10
 	],
 	"../pages/item-detail/item-detail.module": [
-		341,
+		331,
 		9
 	],
 	"../pages/list-master/list-master.module": [
-		331,
+		333,
 		8
 	],
 	"../pages/login/login.module": [
-		332,
+		334,
 		7
 	],
 	"../pages/menu/menu.module": [
-		334,
+		335,
 		6
 	],
 	"../pages/search/search.module": [
-		335,
+		336,
 		5
 	],
 	"../pages/settings/settings.module": [
-		336,
+		337,
 		4
 	],
 	"../pages/signup/signup.module": [
@@ -82,15 +82,15 @@ var map = {
 		3
 	],
 	"../pages/tabs/tabs.module": [
-		339,
+		340,
 		2
 	],
 	"../pages/tutorial/tutorial.module": [
-		340,
+		341,
 		1
 	],
 	"../pages/welcome/welcome.module": [
-		337,
+		339,
 		0
 	]
 };
@@ -392,17 +392,17 @@ var AppModule = /** @class */ (function () {
                     links: [
                         { loadChildren: '../pages/content/content.module#ContentPageModule', name: 'ContentPage', segment: 'content', priority: 'low', defaultHistory: [] },
                         { loadChildren: '../pages/cards/cards.module#CardsPageModule', name: 'CardsPage', segment: 'cards', priority: 'low', defaultHistory: [] },
+                        { loadChildren: '../pages/item-detail/item-detail.module#ItemDetailPageModule', name: 'ItemDetailPage', segment: 'item-detail', priority: 'low', defaultHistory: [] },
+                        { loadChildren: '../pages/item-create/item-create.module#ItemCreatePageModule', name: 'ItemCreatePage', segment: 'item-create', priority: 'low', defaultHistory: [] },
                         { loadChildren: '../pages/list-master/list-master.module#ListMasterPageModule', name: 'ListMasterPage', segment: 'list-master', priority: 'low', defaultHistory: [] },
                         { loadChildren: '../pages/login/login.module#LoginPageModule', name: 'LoginPage', segment: 'login', priority: 'low', defaultHistory: [] },
-                        { loadChildren: '../pages/item-create/item-create.module#ItemCreatePageModule', name: 'ItemCreatePage', segment: 'item-create', priority: 'low', defaultHistory: [] },
                         { loadChildren: '../pages/menu/menu.module#MenuPageModule', name: 'MenuPage', segment: 'menu', priority: 'low', defaultHistory: [] },
                         { loadChildren: '../pages/search/search.module#SearchPageModule', name: 'SearchPage', segment: 'search', priority: 'low', defaultHistory: [] },
                         { loadChildren: '../pages/settings/settings.module#SettingsPageModule', name: 'SettingsPage', segment: 'settings', priority: 'low', defaultHistory: [] },
-                        { loadChildren: '../pages/welcome/welcome.module#WelcomePageModule', name: 'WelcomePage', segment: 'welcome', priority: 'low', defaultHistory: [] },
                         { loadChildren: '../pages/signup/signup.module#SignupPageModule', name: 'SignupPage', segment: 'signup', priority: 'low', defaultHistory: [] },
+                        { loadChildren: '../pages/welcome/welcome.module#WelcomePageModule', name: 'WelcomePage', segment: 'welcome', priority: 'low', defaultHistory: [] },
                         { loadChildren: '../pages/tabs/tabs.module#TabsPageModule', name: 'TabsPage', segment: 'tabs', priority: 'low', defaultHistory: [] },
-                        { loadChildren: '../pages/tutorial/tutorial.module#TutorialPageModule', name: 'TutorialPage', segment: 'tutorial', priority: 'low', defaultHistory: [] },
-                        { loadChildren: '../pages/item-detail/item-detail.module#ItemDetailPageModule', name: 'ItemDetailPage', segment: 'item-detail', priority: 'low', defaultHistory: [] }
+                        { loadChildren: '../pages/tutorial/tutorial.module#TutorialPageModule', name: 'TutorialPage', segment: 'tutorial', priority: 'low', defaultHistory: [] }
                     ]
                 }),
                 __WEBPACK_IMPORTED_MODULE_6__ionic_storage__["a" /* IonicStorageModule */].forRoot()
